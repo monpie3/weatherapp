fb3fc14e00b3cf647c11375cb6b224f5bb38670a
Monika <dotyk_zla@o2.pl>
2020-06-11 23:45:40 +0200

    working on development3

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/src/script.js'));
+app.use(express.static(__dirname + '/'));
 app.listen(process.env.PORT || 8080);
