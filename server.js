01dfd11e51e39de798af911ed87a2333498fddb6
Monika <dotyk_zla@o2.pl>
2020-06-11 22:14:34 +0200

    dependencies update and ready for deployment

--- server.js
+++ server.js
@@ -1,4 +1,28 @@
 var express = require('express');
 var app = express();
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+<<<<<<< HEAD
+app.use(express.static(__dirname + '/src/script.js'));
+=======
+app.use(express.static(__dirname + '/index.html'));
+>>>>>>> bc9fbaa... working on development2
+=======
 app.use(express.static(__dirname + '/'));
+<<<<<<< HEAD
+>>>>>>> fb3fc14... working on development3
+=======
+app.use(express.static(__dirname + '/src/script.js'));
+>>>>>>> d3c220a... working on development 9
+=======
+app.use(express.static(__dirname + '/'));
+>>>>>>> 899d414... working on development 10
+=======
+app.use(express.static(__dirname + '/'));
+>>>>>>> d0f3322... working on development  - back to server file 16
 app.listen(process.env.PORT || 8080);
+=======
+app.listen(process.env.PORT || 8080);
+>>>>>>> 5c40c74... Ready to development
